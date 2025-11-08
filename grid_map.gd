7703cce110a84ecfefda61c01049f6b928f66128
rullinoiz <46585763+rullinoiz@users.noreply.github.com>
2025-11-08 01:13:52 -0800

    we got gridmap slicing by plane

---
+++ grid_map.gd
@@ -0,0 +1,83 @@
+class_name NewGridMap extends GridMap
+
+var size: Vector3i
+
+var smallestX: int = 1000
+var smallestY: int = 1000
+var smallestZ: int = 1000
+
+func get_slice(slice: Vector3i) -> Array[Array]:
+	var objects: Array[Array]
+	if slice.x == 1 or slice.x == -1: # objects[y][z]
+		for y in range(size.y):
+			var array = []
+			array.resize(size.z)
+			objects.append(array)
+	elif slice.y == 1 or slice.y == -1: # objects[x][z]
+		for x in range(size.x):
+			var array = []
+			array.resize(size.z)
+			objects.append(array)
+	elif slice.z == 1 or slice.z == -1: # objects[y][x]
+		for y in range(size.y):
+			var array = []
+			array.resize(size.x)
+			objects.append(array)
+		
+	for v: Vector3i in get_used_cells():
+		var normalized := Vector3i(v.x - smallestX, v.y - smallestY, v.z - smallestZ)
+		if slice.x == 1 or slice.x == -1: 
+			var currentRow = objects[normalized.y]
+			var current: Variant = currentRow.get(normalized.z)
+			
+			if slice.x == 1 and ((not current) or current.x < v.x):
+				currentRow[normalized.z] = v
+			elif slice.x == -1 and ((not current) or current.x > v.x):
+				currentRow[normalized.z] = v
+		elif slice.y == 1 or slice.y == -1:
+			var currentRow = objects[normalized.x]
+			var current: Variant = currentRow.get(normalized.z)
+			
+			if slice.y == 1 and ((not current) or current.y < v.y):
+				currentRow[normalized.z] = v
+			elif slice.y == -1 and ((not current) or current.y > v.y):
+				currentRow[normalized.z] = v
+		elif slice.z == 1 or slice.z == -1:
+			var currentRow = objects[normalized.y]
+			var current: Variant = currentRow.get(normalized.x)
+			
+			if slice.z == 1 and ((not current) or current.z < v.z):
+				currentRow[normalized.x] = v
+			elif slice.z == -1 and ((not current) or current.z > v.z):
+				currentRow[normalized.x] = v
+	
+	return objects
+	
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var largestX: int = -1000
+	var largestY: int = -1000
+	var largestZ: int = -1000
+	
+	for v: Vector3i in get_used_cells():
+		if v.x > largestX:
+			largestX = v.x
+		elif v.x < smallestX:
+			smallestX = v.x
+		
+		if v.y > largestY:
+			largestY = v.y
+		elif v.y < smallestY:
+			smallestY = v.y
+		
+		if v.z > largestZ:
+			largestZ = v.z
+		elif v.z < smallestZ:
+			smallestZ = v.z
+	
+	size = Vector3i(largestX - smallestX + 1, largestY - smallestY + 1, largestZ - smallestZ + 1)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
---
+++ root.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@export var gridContainer: GridContainer
+@export var gridMap: NewGridMap
+@export var rectScene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var slice := gridMap.get_slice(Vector3i(0, 1, 0))
+	gridContainer.columns = slice[0].size()
+	
+	for x: Array in slice:
+		for y in x:
+			var child := rectScene.instantiate() as ColorRect
+			child.color = Color.WHITE if y else Color.BLACK
+			gridContainer.add_child(child)
+	
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
