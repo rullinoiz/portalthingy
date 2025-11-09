e2aa5f68b79febcc0bf608b403def7ba7ae0f027
FabulousFab101 <fabisdiaz2006@gmail.com>
2025-11-09 14:16:42 -0800

    title and credits

---
+++ title_screen.gd
@@ -0,0 +1,20 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	if not bgmplayer.playing:
+		bgmplayer.play() # plays music across whole game
+	pass	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass	
+
+func _on_play_pressed():
+	$click.play()
+	get_tree().change_scene_to_file("res://levels/root.tscn")
+
+func _on_credits_pressed():
+	$click.play()
+	get_tree().change_scene_to_file("res://credits.tscn")
