14873a561e62844062429795fba517e69c7f1aed
rullinoiz <46585763+rullinoiz@users.noreply.github.com>
2025-11-10 12:45:05 -0800

    ok this really is the final version stop changing stuff

--- levels/credits.gd
+++ levels/credits.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
+var beginMoving: bool = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	await get_tree().create_timer(2).timeout
+	beginMoving = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
+	if beginMoving and position.y < 318:
+		position.y += 150 * delta
 	pass
