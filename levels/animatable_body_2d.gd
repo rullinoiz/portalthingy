ecd833ea94188cb7137af40f2fd350f2d5414810
Dicanio Darren Liong <20587979@fhda.edu>
2025-11-09 19:17:21 -0800

    rooftop scene

--- levels/animatable_body_2d.gd
+++ levels/animatable_body_2d.gd
@@ -1,12 +1,13 @@
-extends Node2D
+extends AnimatableBody2D
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Wind.stop()
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	animation_player.play("credits_Moving")
 	pass
