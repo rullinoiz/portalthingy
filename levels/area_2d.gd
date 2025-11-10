98de13db1f9c7cdd8411bc89a370efe54c55bdb4
Dicanio Darren Liong <20587979@fhda.edu>
2025-11-09 20:06:27 -0800

    ctuscene

--- levels/area_2d.gd
+++ levels/area_2d.gd
@@ -3,5 +3,5 @@ extends Area2D
 
 
 func _on_body_entered(body: PhysicsBody2D) -> void:
-	get_tree().change_scene_to_file("res://levels/bro jumps.tscn")
+	get_tree().change_scene_to_file("res://levels/fallingCutscene.tscn")
 	pass # Replace with function body.
---
+++ levels/falling_cutscene.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$fallingCutscene.play()
+	pass # Replace with function body.
+
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_falling_cutscene_finished() -> void:
+	get_tree().change_scene_to_file("res://levels/fallingCutscene.tscn")
+	pass # Replace with function body.
