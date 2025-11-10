cd40f504b45a01d50b1915f40579da90910c1006
Dicanio Darren Liong <20587979@fhda.edu>
2025-11-09 19:38:29 -0800

    sdfhsdfgsdfg

--- ending.gd
+++ ending.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#ddddddddddddddddddddddd$Wind.stop()
+	#$Wind.stop()
 	pass # Replace with function body.
 
 
--- levels/area_2d.gd
+++ levels/area_2d.gd
@@ -3,5 +3,5 @@ extends Area2D
 
 
 func _on_body_entered(body: PhysicsBody2D) -> void:
-	get_tree().change_scene_to_file("res://levels/ending.tscn")
+	get_tree().change_scene_to_file("res://levels/bro jumps.tscn")
 	pass # Replace with function body.
